33caebb877bed26032a12fadc0d9fd8bef29f557
Shaisan2 <shivam0sharma@icloud.com>
2017-05-26 18:42:08 -0400

    Created the BlueButton class

---
+++ Custom View/BlueButton.swift
@@ -0,0 +1,26 @@
+//
+//  BlueButton.swift
+//  Custom View
+//
+//  Created by Shivam Sharma on 5/26/17.
+//  Copyright © 2017 ShivamSharma. All rights reserved.
+//
+
+import UIKit
+
+class BlueButton: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 5.0
+        backgroundColor = UIColor(red: 46.0/255.0, green: 135.0/255.0, blue: 195.0/255.0, alpha: 1.0)
+        setTitleColor(UIColor.white, for: .normal)
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
